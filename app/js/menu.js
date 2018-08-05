5a50ff66d0a7cd31990496755ca000b5f26ea9d5
Nogtkov <annogotkov@yandex.ru>
2018-08-05 12:46:22 +0300

    some changes

--- app/js/menu.js
+++ app/js/menu.js
@@ -1,10 +1,35 @@
 $(document).ready(function() {
-	var menuItem = $('.left-column__menu__link');
+	var menuItem = $('.left-column__menu__link'),
+		burgerMenu = $('.toggle-menu__burger-btn');
+
+	$(window).on('scroll', function() {
+		var scroll = $(window).scrollTop();
+
+		if(scroll < ($('.about').offset().top)/2) {
+			toActivate(menuItem, 0);
+		} else if(scroll >= ($('.about').offset().top)/2) {
+			toActivate(menuItem, 1);
+		}
+
+	});
 
 	menuItem.on('click', function(e) {
 		e.preventDefault();
 		menuItem.removeClass('left-column__menu__link_state_active');
 		$(this).toggleClass('left-column__menu__link_state_active');
+		var id = $(this).attr('id');
+		$('html, body').animate({scrollTop: $(id).offset().top}, 1500);
+	});
+
+	burgerMenu.on('click', function(e) {
+		e.preventDefault();
+		$(this).toggleClass('toggle-menu__burger-btn_state_active');
+		$(this).next().toggleClass('toggle-menu__menu-wrapper_state_active');
 	});
 
 });
+
+function toActivate(element, index) {
+	element.removeClass('left-column__menu__link_state_active');
+	element.eq(index).toggleClass('left-column__menu__link_state_active');
+}
